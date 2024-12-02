c4e769933379cf95ab53652c19df05f46a912091
unknown <hophap1311@gmail.com>
2024-12-03 06:23:07 +0700

    manage product

--- src/api/editService.ts
+++ src/api/editService.ts
@@ -1,46 +1,50 @@
 import * as request from "../utils/request";
 
 interface Product {
-    name: string,
-    basePrice: number,
-    stock: number,
-    description: string,
-    discountPercentage: number
+  name: string;
+  basePrice: number;
+  stock: number;
+  description: string;
+  discountPercentage: number;
 }
 
 interface Category {
-    name: string
+  name: string;
 }
 
 export const uploadImage = async (id: string, formData: {}) => {
-    try {
-        const response = await request.patch(
-            `/product/picture/${id}`,
-            formData,
-        );
-        return response.data;
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const response = await request.patch(`/product/picture/${id}`, formData);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-export const editProduct = async (id: string, body: Product) => {    
-    try {
-        const response = await request.patch(
-            `/product/${id}`,
-            body
-        );
-        return response.data
-    } catch (error) {
-        console.log(error);
-    }
+export const editProduct = async (id: string, body: any) => {
+  try {
+    const response = await fetch(
+      `http://localhost:5000/api/v1/products/${id}`,
+      {
+          headers: {
+            "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+        method: "PATCH",
+        body: JSON.stringify(body),
+      }
+    );
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const editCategory = async (id: string, body: Category) => {
-    try {
-        const response = await request.patch(`/category/${id}`, body)
-        return response.data
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    const response = await request.patch(`/category/${id}`, body);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
