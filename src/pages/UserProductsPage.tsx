1591d3761123c33e945efe28f6776ece73ce116c
unknown <hophap1311@gmail.com>
2024-12-08 11:26:44 +0700

    remove unnecessary files

--- src/pages/UserProductsPage.tsx
+++
@@ -1,9 +0,0 @@
-import { UserHeader } from "../components/header/UserHeader";
-import { UserProductList } from "../components/lists/UserProductList";
-
-export const UserProductsPage = () => {
-    return <div className="dark:bg-[#292a30] min-h-screen">
-        <UserHeader />
-        <UserProductList />
-    </div>
-};
